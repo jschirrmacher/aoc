9f2fe2ca4dd5fc1b8d2c98a7b56057385593c5dd
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2023-12-02 11:08:12 +0100

    fix part 2 of day 1

--- 2023/1/part2.ts
+++ 2023/1/part2.ts
@@ -1,22 +1,35 @@
 const digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-const pattern = new RegExp("(" + digits.join("|") + ")", "g")
+const pattern = new RegExp("(" + digits.join("|") + "|\\d" + ")")
 
 export default function (rawData: string) {
   return rawData
     .split("\n")
     .filter(line => line)
-    .map(line => line.replace(pattern, replaceSpelledOutDigit))
-    .map(line =>
-      line
-        .split("")
-        .filter(c => c.match(/\d/))
-        .join("")
-    )
-    .map(line => line[0] + line[line.length - 1])
+    .map(line => findFirstDigit(line) + findLastDigit(line))
     .map(Number)
     .reduce((sum, val) => sum + val, 0)
 }
 
-function replaceSpelledOutDigit(digit: string) {
-  return digits.findIndex(d => d === digit).toString()
+function findFirstDigit(line: string) {
+  const match = line.match(pattern)
+  if (match) {
+    const index = digits.findIndex(d => d === match[0])
+    if (index >= 0) {
+      return index.toString()
+    }
+    return match[0]
+  }
+  return ""
+}
+
+function findLastDigit(line: string) {
+  let pos = line.length
+  while (--pos >= 0) {
+    const sub = line.substring(pos)
+    const digit = findFirstDigit(sub)
+    if (digit) {
+      return digit
+    }
+  }
+  return ""
 }
