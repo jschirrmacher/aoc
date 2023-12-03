1063b47050eb66ba4bcad56c76080e3aa80cc017
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2023-12-03 16:45:45 +0100

    Part 1 of day 2

--- 2023/2/part1.ts
+++ 2023/2/part1.ts
@@ -1,3 +1,40 @@
+const bag = {
+  red: 12,
+  green: 13,
+  blue: 14,
+}
+
+type Bag = typeof bag
+type Color = keyof Bag
+type CubeSet = [number, Color][][]
+
 export default function (rawData: string) {
-  return 0
+  const games = rawData
+    .split("\n")
+    .filter(l => l)
+    .map(parseGame)
+    .filter(possible)
+
+  return games.map(([game]) => game).reduce((sum, game) => sum + game, 0)
+}
+
+function parseGame(line: string) {
+  const { game, cubes } = line.match(/Game (?<game>\d+): (?<cubes>.*)/)!.groups!
+  return [
+    +game,
+    cubes.split(";").map(value =>
+      value
+        .split(",")
+        .map(v => v.trim().split(" "))
+        .map(([num, color]) => [+num, color as Color])
+    ),
+  ] as [number, CubeSet]
+}
+
+function possible([, cubeSet]: [number, CubeSet]) {
+  return cubeSet.every(cubes => cubes.every(canBeInBag))
+}
+
+function canBeInBag([num, color]: [number, Color]) {
+  return num <= bag[color]
 }
--- index.ts
+++ index.ts
@@ -4,17 +4,26 @@ import { hrtime } from "process"
 
 type SolveFunc = (rawData: string) => number | string | Promise<number | string>
 
-async function runModule(part: number, dir: string, inputFile: string, module: { default: SolveFunc }) {
+async function runModule(
+  part: number,
+  dir: string,
+  inputFile: string,
+  module: { default: SolveFunc }
+) {
   const input = resolve(dir, inputFile)
   if (existsSync(input)) {
     const data = readFileSync(input, "utf8")
     const start = hrtime.bigint()
-    const result = await module.default(data)
-    console.log(
-      `Result of part ${part} on day ${day.getDate()} (${inputFile}): ${result} (${
-        (hrtime.bigint() - start) / 1_000_000n
-      }ms)`
-    )
+    try {
+      const result = await module.default(data)
+      console.log(
+        `Result of part ${part} on day ${day.getDate()} (${inputFile}): ${result} (${
+          (hrtime.bigint() - start) / 1_000_000n
+        }ms)`
+      )
+    } catch (error) {
+      console.error(error)
+    }
   } else {
     console.log(`No ${inputFile} defined for day ${day.getDate()}`)
   }
@@ -27,7 +36,7 @@ async function run(part: number) {
     }
     return `${baseName}.txt`
   }
-  
+
   const fileName = resolve(dir, `part${part}.ts`)
   if (existsSync(fileName)) {
     const module = await import(fileName)
@@ -46,8 +55,14 @@ if (day.getDate() < 26 && day.getMonth() === 11) {
     mkdirSync(dir, { recursive: true })
     writeFileSync(join(dir, "input.txt"), "")
     writeFileSync(join(dir, "testdata.txt"), "")
-    writeFileSync(join(dir, "part1.ts"), "export default function (rawData: string) {\n  return 0\n}\n")
-    writeFileSync(join(dir, "part2.ts"), "export default function (rawData: string) {\n  return 0\n}\n")
+    writeFileSync(
+      join(dir, "part1.ts"),
+      "export default function (rawData: string) {\n  return 0\n}\n"
+    )
+    writeFileSync(
+      join(dir, "part2.ts"),
+      "export default function (rawData: string) {\n  return 0\n}\n"
+    )
   }
 }
 
