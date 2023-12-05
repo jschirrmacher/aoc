1a2a644f19197e6e22f0e36b670ea8662fa094ab
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2023-12-05 22:18:35 +0100

    Part 2 of day 4

--- 2023/4/part2.ts
+++ 2023/4/part2.ts
@@ -1,3 +1,19 @@
 export default function (rawData: string) {
-  return 0
+  const lines = rawData.split("\n")
+  const amounts = lines
+    .filter(row => row)
+    .map(row => row.split(":"))
+    .map(([, numbers]) => numbers.split("|").map(numbers => numbers.split(" ").filter(num => num)))
+    .map(([winning, having]) => having.filter(num => winning.includes(num)))
+    .map(card => card.length)
+    .reduce(
+      (amounts, num, cardNo) =>
+        amounts.map((amount, index) => {
+          const inNum = index > cardNo && index <= cardNo + num
+          return amount + (inNum ? amounts[cardNo] : 0)
+        }),
+      Array(lines.length).fill(1)
+    )
+
+  return amounts.reduce((acc, val) => acc + val, 0)
 }
