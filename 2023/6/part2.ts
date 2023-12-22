207d1b0289456779643393e0560caede30a67588
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2023-12-22 15:23:23 +0100

    Part 2 of day 6

--- 2023/6/part2.ts
+++ 2023/6/part2.ts
@@ -1,3 +1,20 @@
 export default function (rawData: string) {
-  return 0
+  const [time, distance] = rawData
+    .split("\n")
+    .map(line => line.match(/.*:\s*([\d\s]+)/))
+    .map(m => m?.at(1))
+    .map(m => m?.replaceAll(/\s/g, ""))
+    .map(Number)
+
+  return getNumberOfWaysToWin(time, distance)
+}
+
+function getNumberOfWaysToWin(time: number, distance: number) {
+  return [...Array(time)].filter(
+    (_, index) => index >= 14 && index <= time - 14 && getDistance(index, time - index) > distance
+  ).length
+}
+
+function getDistance(pressDuration: number, runDuration: number) {
+  return pressDuration * runDuration
 }
