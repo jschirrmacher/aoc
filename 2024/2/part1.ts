7f3e20c36e7360439a06917ebd47018b14bda76d
Joachim Schirrmacher <joachim.schirrmacher@gmail.com>
2024-12-02 06:44:04 +0100

    day2/2024

--- 2024/2/part1.ts
+++ 2024/2/part1.ts
@@ -1,3 +1,10 @@
 export default function (rawData: string) {
-  return 0
+  const reports = rawData
+    .split("\n")
+    .map(row => row.split(" ").map(Number))
+    .map(row => row.slice(1).map((num, index) => num - row[index]))
+    .filter(row => row.every(num => Math.sign(num) === Math.sign(row[0])))
+    .filter(row => row.every(num => [1, 2, 3].includes(Math.abs(num))))
+
+  return reports.length
 }
--- 2024/2/part2.ts
+++ 2024/2/part2.ts
@@ -1,3 +1,18 @@
 export default function (rawData: string) {
-  return 0
+  const reports = rawData
+    .split("\n")
+    .map(row => row.split(" ").map(Number))
+    .filter(report =>
+      report.some((_, i) => {
+        const filtered = report.filter((_, j) => j !== i)
+        const differences = filtered.slice(1).map((num, index) => num - filtered[index])
+        const sign = Math.sign(differences[0])
+        return (
+          differences.every(num => Math.sign(num) === sign) &&
+          differences.every(num => [1, 2, 3].includes(Math.abs(num)))
+        )
+      })
+    )
+
+  return reports.length
 }
