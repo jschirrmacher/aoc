cfdb39c8864f9c463935e6237087522b1a8440ad
Joachim Schirrmacher <joachim.schirrmacher@gmail.com>
2024-12-09 18:03:38 +0100

    day7/2024

---
+++ 2024/7/part2.ts
@@ -0,0 +1,52 @@
+const operators = ["+", "*", "||"]
+
+export default function (rawData: string) {
+  function evaluateEquation(numbers: number[], target: number): boolean {
+    const numOperators = numbers.length - 1
+
+    function evaluate(ops: string[]): number {
+      return numbers.reduce((acc, num, index) => {
+        if (index === 0) return num
+        switch (ops[index - 1]) {
+          case "+":
+            return acc + num
+          case "*":
+            return acc * num
+          case "||":
+            return Number(`${acc}${num}`)
+          default:
+            throw new Error("Invalid operator")
+        }
+      }, 0)
+    }
+
+    function generateCombinations(ops: string[], depth: number): boolean {
+      if (depth === numOperators) {
+        return evaluate(ops) === target
+      }
+      return (
+        operators.filter(operator => {
+          const newOps = [...ops, operator]
+          return generateCombinations(newOps, depth + 1)
+        }).length > 0
+      )
+    }
+
+    return generateCombinations([], 0)
+  }
+
+  return rawData
+    .trim()
+    .split("\n")
+    .map(line => line.split(":"))
+    .map(([target, numbers]) => ({
+      target: Number(target.trim()),
+      numbers: numbers.trim().split(" ").map(Number),
+    }))
+    .reduce((acc, eq) => {
+      if (evaluateEquation(eq.numbers, eq.target)) {
+        return acc + eq.target
+      }
+      return acc
+    }, 0)
+}
